1aabd0d6f812f4bbdbdb1c9a6e7f06569986f4e6
Muiz Ud Din <mueezgamings@gmail.com>
2025-12-25 21:40:48 +0500

    Fix Terms of Service and Privacy Policy pages to scroll to top when loaded

--- frontend/src/pages/TermsOfService.tsx
+++ frontend/src/pages/TermsOfService.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 
 const TermsOfService: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-4xl mx-auto">
